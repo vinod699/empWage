37f0b7b8c580796802e6b62110373741d0b71c8e
vinod699 <kosarajuvinod9999@gmail.com>
2021-03-23 15:27:23 +0530

    Added: emp Wage Uc1 prbolem

--- src/empWage/empwage.java
+++ src/empWage/empwage.java
@@ -3,7 +3,12 @@ package empWage;
 public class empwage {
 
 	public static void main(String[] args) {
-		System.out.println("Welcom to emp Wage");
+		int IS_FULL_TIME=1;
+		double empCheck=Math.floor(Math.random() * 10) % 2;
+		if (empCheck == IS_FULL_TIME)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Abscent");
 
 	}
 
