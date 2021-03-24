c751e2b78c00a725c59fcbbf2d2b53fa45f441bb
vinod699 <kosarajuvinod9999@gmail.com>
2021-03-24 12:22:55 +0530

    uc5empwage

--- src/empWage/empwage.java
+++ src/empWage/empwage.java
@@ -3,11 +3,13 @@ public class empWage {
 	public static final int IS_PART_TIME=1;
 	public static final int IS_FULL_TIME=2;
 	public static final int WAGE_PER_HR=20;
+	public static final int NUM_OF_WORKING_DAYS=2;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//Variable
-		int empHrs=0;
+		int empHrs=0,totalEmpWage=0,empWage=0;
+		for(int day=0;day<NUM_OF_WORKING_DAYS;day++) {
 		int empCheck = (int) Math.floor(Math.random() * 10)%3;
 		switch(empCheck) {
 		case IS_PART_TIME:
@@ -19,7 +21,10 @@ public class empWage {
 		default:
 			empHrs=0;
 		}
-		System.out.println("Employee Wage : " + WAGE_PER_HR * empHrs );
+		empWage=empHrs*WAGE_PER_HR;
+		totalEmpWage += empWage;
+		System.out.println("Employee Wage : " + empWage );
 	}
-
+	System.out.println("Total Employee Wage : "+ totalEmpWage);
+}
 }
